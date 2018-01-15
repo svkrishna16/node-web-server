e5ad14330ecd0ea4f993618bd762aa4ee64eeb4b
Vijay Siricilla <svkrishna16@gmail.com>
2018-01-15 20:42:49 +1300

    Add Projects Page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const port = process.env.port || 3000;
+var userName = require("os").userInfo().username;
 
 var app = express();
 
@@ -8,6 +9,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear();
 });
+
 app.set('view engine','hbs');
 //setup your middleware
 //app.use(express.static(__dirname + '/public'));
@@ -23,8 +25,7 @@ app.get('/',(req,res)=>{
   res.render('home.hbs', {
     pageTitle: 'Vijay\'s Page',
     pageHeader: 'Welcome to Node.js Home',
-    author: 'Kaashvi Siricilla',
-    neverused: 'Kaashvi Siricilla'
+    author: 'Kaashvi Siricilla'
   });
 });
 
@@ -33,9 +34,19 @@ app.get('/about',(req,res)=>{
   res.render('about.hbs', {
     pageTitle: 'About Page',
     pageHeader: 'Node.js About Page',
+    aboutPageTxt: 'Node.js built on Chrome\'s JavaScript runtime. It uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive applications that run across distributed devices.',
     author: 'Vijay Siricilla'
   });
 });
+
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects Page',
+    pageHeader: 'Node.js Projects:',
+    projectsPageText: 'Popular Node.js projects - PayPal, Netflix, Uber, LinkedIn, eBay, Walmart, NASA, Mozilla, Trello',
+    author: `${userName}`.toLocaleUpperCase()
+  });
+})
 
 
 app.listen(port, ()=>{
